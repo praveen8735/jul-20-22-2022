fd806183602fc274ea5fd9fd89b60ade720183b0
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-07-21 11:23:13 +0530

    Day02

---
+++ day02/ho_functions_2.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	var add = func(x int, y int) int {
+		return x + y
+	}
+	var sub = func(x int, y int) int {
+		return x - y
+	}
+	var multiply = func(x int, y int) int {
+		return x * y
+	}
+	var square = func(x int) int { return x * x }
+	//doSomeMath(square)
+	doSomeMath(add)
+	doSomeMath(sub)
+	doSomeMath(multiply)
+	doSomeMath(func(x int, y int) int { return x / y })
+}
+
+type mathFunction func(int, int) int
+
+func doSomeMath(operation mathFunction) {
+	//func doSomeMath(operation func(int, int) int) {
+	var num1 int = 10
+	var num2 int = 20
+	fmt.Println(operation(num1, num2))
+}
+
+/*
+//OLD STYLE
+func main() {
+	fmt.Println(add(num1, num2))
+	fmt.Println(sub(num1, num2))
+	fmt.Println(multiply(num1, num2))
+}
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func sub(x int, y int) int {
+	return x - y
+}
+
+func multiply(x int, y int) int {
+	return x * y
+}
+*/
