fc6fb366c0ee61eebbe37714ebd6831dd3c31636
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-07-22 13:33:05 +0530

    Day03

---
+++ day03/lab09.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type Stock struct {
+	Symbol string
+	Cmp float32
+}
+
+var symbols = []string { "HP", "GOOG", "FB", "WIPRO", "INFY", "DELL", "HUL", "ITC", "RELIND", "NETFLIX", "AMAZON" }
+var baseUrl = "http://healthycoder.in/stocks.php?symbol="
+var stockList = []Stock{}
+var wg = sync.WaitGroup{}
+var stockOutputChannel = make(chan []byte, 1)
+
+func main()  {
+	wg.Add(len(symbols))
+	for _, symbol := range symbols {
+		go getCurrentMarketPriceFromServer(symbol)
+	}
+	go processJsonOutput()
+	wg.Wait()
+	printStockJson()
+}
+
+func printStockJson() {
+	contents, _ := json.Marshal(stockList)
+	log.Println(string(contents))
+}
+
+func getCurrentMarketPriceFromServer(symbol string) {
+	url := baseUrl + symbol
+	log.Println("Fetching data for " + symbol + " ...")
+	response, _ := http.Get(url)
+	output, _ := ioutil.ReadAll(response.Body)
+	defer func() {
+		response.Body.Close()
+	}()
+	stockOutputChannel <- output
+
+}
+func processJsonOutput() {
+	for len(stockList) != len(symbols) {
+		jsonOutput := <- stockOutputChannel
+		stock := Stock{}
+		json.Unmarshal(jsonOutput, &stock)
+		log.Println("Processing " + string(jsonOutput))
+		stockList = append(stockList, stock)
+		wg.Done()
+	}
+}
